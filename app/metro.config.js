cb76fa2aa7cf37a5dfa4c7d24d60e29b1c8c0fcd
MihaiFilca <120052866+misu1403@users.noreply.github.com>
2025-06-27 23:17:16 +0300

    Create metro.config.js

---
+++ app/metro.config.js
@@ -0,0 +1,10 @@
+
+const { getDefaultConfig } = require('@expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+config.resolver.extraNodeModules = {
+  stream: require.resolve('stream-browserify'),
+};
+
+module.exports = config;
